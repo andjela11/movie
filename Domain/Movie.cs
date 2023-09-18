b591b3329088905c9e60d291417d5aa42b156e7c
Andjela Jovanovic <andjela.jovanovic@invpc007l.vega.local>
2023-09-18 10:42:56 +0200

    first commit

---
+++ Domain/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Director { get; set; }
+    public string Actor { get; set; }
+}
