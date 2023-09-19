a0c188ce66dea3ab1787aa15c723a3beca1b99e8
andjela.jovanovic <andjela1198@gmail.com>
2023-09-19 16:44:40 +0200

    removed unnecessary using

--- Persistence/PersistenceServiceCollectionExtensions.cs
+++ Persistence/PersistenceServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using Application.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
