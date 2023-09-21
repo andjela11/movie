96e7916f2b5d1499a97ea712ba94c875ee13e41a
andjela11 <65898647+andjela11@users.noreply.github.com>
2023-09-21 17:16:59 +0200

    Delete Infrastructure directory

--- Infrastructure/Persistence/DataConetxt.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Persistence;
-
-public class DataContext : DbContext
-{
-    public DbSet<Movie> Movies => Set<Movie>();
-    public DataContext(DbContextOptions options) : base(options)
-    {
-    }
-}
