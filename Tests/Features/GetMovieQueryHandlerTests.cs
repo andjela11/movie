fff4c792253097ed2edbf5c9123ee9638ca35090
andjela11 <65898647+andjela11@users.noreply.github.com>
2023-09-21 14:24:43 +0200

    Test get movie feature (#5)

--- Tests/Features/GetMovieQueryHandlerTests.cs
+++ Tests/Features/GetMovieQueryHandlerTests.cs
@@ -1,11 +1,13 @@
-﻿using Application.Features.GetMovie;
+﻿using Application.Exceptions;
+using Application.Features.GetMovie;
 using Application.Interfaces;
 
 using Domain;
-
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 
 using Moq;
+using Moq.EntityFrameworkCore;
 
 namespace Tests.Features;
 
@@ -13,41 +15,58 @@ public class GetMovieQueryHandlerTests
 {
     private GetMovieQueryHandler systemUnderTest;
     private Mock<IDataContext> dataContextMock;
-    private Mock<DbSet<Movie>> moviesMock;
 
     [SetUp]
-    public void Setup()
-    {
-        dataContextMock = new Mock<IDataContext>();
-        moviesMock = new Mock<DbSet<Movie>>();
-    }
+    public void Setup() => dataContextMock = new Mock<IDataContext>();
 
     // pattern: AAA
     // A: Arrange
     // A: Act
     // A: Assert
 
-    // [Test]
-    // public async Task<Movie> GetMovie_TwoMoviesWithSameIdExists_ShouldThrowInvalidOperationExceptionAsync()
-    // {
-    //     // Arrange
-    //     var movies = new List<Movie>
-    //     {
-    //         new()
-    //         {
-    //             Id = 1,
-    //             Title = "Rambo"
-    //         },
-    //         new()
-    //         {
-    //             Id = 1,
-    //             Title = "Rambo 2"
-    //         }, 
-    //     };
-    //
-    //     moviesMock.As<IQueryable<Movie>>().Setup(x => x.Pr);
-    //     dataContextMock.Object.Movies = moviesMock;
-    //     
-    //     await dataContextMock.Movies.AddRange(movies)
-    // }
+    [Test]
+    public void GetMovieQuery_TwoMoviesWithSameIdExists_ShouldThrowMoreThanOneMovieException()
+    {
+        // Arrange
+        const int movieId = 1;
+        var movies = new List<Movie>
+        {
+            new()
+            {
+                Id = movieId,
+                Title = "Rambo"
+            },
+            new()
+            {
+                Id = movieId,
+                Title = "Rambo 2"
+            }, 
+        };
+        dataContextMock.Setup(x => x.Movies).ReturnsDbSet(movies);
+
+        systemUnderTest = new GetMovieQueryHandler(dataContextMock.Object);
+        
+        // Act && Assert
+        systemUnderTest.Invoking(x => x.Handle(new GetMovieQuery(movieId), new CancellationToken()))
+            .Should().ThrowAsync<MoreThanOneMovieException>()
+            .WithMessage("This shouldn't be happening");
+    }
+
+    [Test]
+    // {ActionName}_{TestScenario}_{ShouldHappen}
+    public async Task GetMovieQuery_FindById_ShouldReturnMovieAsync()
+    {
+        // Arrange
+        var movies = new List<Movie> { new() { Id = 4, Title = "Rambo" } };
+
+        dataContextMock.Setup(x => x.Movies).ReturnsDbSet(movies);
+
+        systemUnderTest = new GetMovieQueryHandler(this.dataContextMock.Object);
+
+        // Act
+        var result = await systemUnderTest.Handle(new GetMovieQuery(4), new CancellationToken());
+
+        // Assert
+        result.Should().BeSameAs(movies.First());
+    }
 }
